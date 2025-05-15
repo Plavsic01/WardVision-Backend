ee979d0bfd245801158b4ae322bb9f3381247541
Andrej Plavsic <andrejplavsic3@gmail.com>
2025-05-15 13:44:49 +0200

    Riot verification proccess

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ app.use("/api/match-history", matchHistory);
 app.use("/api/champions", champions);
 app.use("/api/items", items);
 app.use("/api/spells", spells);
+app.use(express.static("public"));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
